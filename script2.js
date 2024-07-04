d70d9053debc8013552bc3e648f17d6f5559eaa0
Micael <Micaelrespoki@gmail.com>
2024-07-04 19:00:44 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,14 @@
+
+function AbrirpopUp(){
+
+    var Exercicio2 = ("Exercicio2 Está funcionando?");
+    
+    var confirm = window.confirm("Exercicio2 Está funcionando?");
+    
+    if (confirm){
+    alert("Sim, ela está Funcionando!")
+    }
+    else{
+    alert("Você cancelou ou Não Respondeu a Pesquisa!");  
+    }
+    }
